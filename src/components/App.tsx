af7e41ddd31f2c199d3822f886b9882707efdbbd
YZarytskyi <yurazar222@gmail.com>
2022-12-25 11:39:52 +0200

    React 4 module

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,64 +1,51 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Contact } from '../types/types';
 import ContactsList from './ContactsList/ContactsList';
 import ContactsForm from './ContactsForm/ContactsForm';
 import Filter from './Filter/Filter';
 import { ContactsTitle, PageTitle } from './App.styled';
 
-export interface AppState {
-  contacts: Array<Contact>;
-  filter: string;
-}
 
-export default class App extends Component<{}, AppState> {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+const contactsData = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
+
+const App = () => {
+  const [contacts, setContacts] = useState<Array<Contact>>(contactsData);
+  const [filter, setFilter] = useState<string>('');
+
+  const setNewContact = (contact: Contact) => {
+    setContacts(prev => [contact, ...prev]);
   };
 
-  setNewContact = (contact: Contact) => {
-    this.setState(prev => ({
-      contacts: [contact, ...prev.contacts],
-    }));
+  const deleteContact = (id: string) => {
+    setContacts(prev => prev.filter(contact => contact.id !== id));
   };
 
-  deleteContact = (id: string) => {
-    this.setState(prev => ({
-      contacts: prev.contacts.filter(el => el.id !== id),
-    }));
+  const filterContacts = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilter(e.currentTarget.value)
   };
 
-  filterContacts = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.currentTarget.value;
-    this.setState({
-      filter: value,
-    });
-  };
+  return (
+    <div>
+      <PageTitle>Phonebook</PageTitle>
+      <ContactsForm
+        setNewContact={setNewContact}
+        contacts={contacts}
+      />
+      <ContactsTitle>Contacts</ContactsTitle>
+      <Filter filter={filter} filterContacts={filterContacts} />
+      <ContactsList
+        contacts={contacts}
+        filter={filter}
+        deleteContact={deleteContact}
+      />
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div>
-        <PageTitle>Phonebook</PageTitle>
-        <ContactsForm
-          setNewContact={this.setNewContact}
-          contacts={this.state.contacts}
-        />
-        <ContactsTitle>Contacts</ContactsTitle>
-        <Filter
-          filter={this.state.filter}
-          filterContacts={this.filterContacts}
-        />
-        <ContactsList
-          contacts={this.state.contacts}
-          filter={this.state.filter}
-          deleteContact={this.deleteContact}
-        />
-      </div>
-    );
-  }
-}
+export default App;
